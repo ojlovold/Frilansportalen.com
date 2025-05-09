2795d0790070d81e63e72a2a3e82c18207cba97a
ojlovold <ojlovold@gmail.com>
2025-05-09 14:54:59 +0200

    Create LoginHelper.tsx

---
+++ components/LoginHelper.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function LoginHelper() {
+  const handleLogin = () => {
+    localStorage.setItem("user_email", "admin@frilansportalen.com");
+    alert("Logget inn som admin!");
+  };
+
+  return (
+    <button onClick={handleLogin} className="bg-black text-white px-4 py-2 rounded">
+      Logg inn som admin
+    </button>
+  );
+}
