4015673448447ad119bd827e8497861ee47884f1
ojlovold <ojlovold@gmail.com>
2025-05-10 15:08:23 +0200

    Update system.tsx

--- pages/system.tsx
+++ pages/system.tsx
@@ -1,7 +1,27 @@
 import Head from "next/head";
 import Layout from "../components/Layout";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { supabase } from "../utils/supabaseClient";
 
 export default function Systemstatus() {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    const sjekkInnlogging = async () => {
+      const { data } = await supabase.auth.getUser();
+      if (!data.user) {
+        router.push("/login");
+      } else {
+        setLoading(false);
+      }
+    };
+    sjekkInnlogging();
+  }, [router]);
+
+  if (loading) return <Layout><p className="text-sm">Laster systemstatus...</p></Layout>;
+
   const produksjonAktiv = true;
   const backupAktiv = true;
   const vipps = false;
