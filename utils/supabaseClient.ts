f2da3a301d29a01cc0367dc2d3c390d75c3cf87c
ojlovold <ojlovold@gmail.com>
2025-05-10 20:43:15 +0200

    Update supabaseClient.ts

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabase = createClient(
+  "https://tvnwbchnvnhehuzrzqfq.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2bndiY2hudm5oZWh1enJ6cWZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3OTU3ODQsImV4cCI6MjA2MjM3MTc4NH0.jkjwXAglHkSeOmCDdvAIq_cJnqjn-kkrvEWLWLEn8as"
+);
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export { supabase };
