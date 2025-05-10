480efbc1ff5fdb8487ebc16206c41393731ad9cd
ojlovold <ojlovold@gmail.com>
2025-05-10 21:05:58 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,10 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      colors: {
-        portalGul: "#FFF200",
-        portalSvart: "#000000"
-      },
-      fontFamily: {
-        sans: ['Arial', 'sans-serif']
-      }
-    }
+    extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};
