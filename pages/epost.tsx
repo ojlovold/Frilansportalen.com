b54c8cc8f85c603aed9f7199178a53de74ab2090
ojlovold <ojlovold@gmail.com>
2025-05-11 12:42:40 +0200

    Update epost.tsx

--- pages/epost.tsx
+++ pages/epost.tsx
@@ -2,17 +2,20 @@ import Head from "next/head";
 import { useUser } from "@supabase/auth-helpers-react";
 import Dashboard from "@/components/Dashboard";
 import EpostInnboks from "@/components/epost/Innboks";
+import NyEpost from "@/components/epost/NyEpost";
 
 export default function EpostSide() {
   const user = useUser();
-  if (!user) return <p>Du må være innlogget for å se e-postene dine.</p>;
+  if (!user) return <p>Du må være innlogget for å bruke e-postsystemet.</p>;
 
   return (
     <Dashboard>
       <Head>
         <title>E-post | Frilansportalen</title>
       </Head>
-      <div className="space-y-6">
+
+      <div className="space-y-8">
+        <NyEpost fraId={user.id} />
         <EpostInnboks brukerId={user.id} />
       </div>
     </Dashboard>
