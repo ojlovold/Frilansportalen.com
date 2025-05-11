dde0d58263a1dc4098fd223b8a5abcc7ae924d0b
ojlovold <ojlovold@gmail.com>
2025-05-11 22:25:03 +0200

    Update Map.tsx

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,21 +1,39 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+// components/Map.tsx
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import { useEffect } from 'react'
 
-export default function Map({ markører }: { markører: any[] }) {
+// Fiks for manglende ikoner i produksjon
+delete (L.Icon.Default.prototype as any)._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: '/leaflet/marker-icon-2x.png',
+  iconUrl: '/leaflet/marker-icon.png',
+  shadowUrl: '/leaflet/marker-shadow.png',
+})
+
+type MapProps = {
+  markører: { lat: number; lng: number; tekst: string }[]
+}
+
+export default function Map({ markører }: MapProps) {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      import('leaflet')
+    }
+  }, [])
+
   return (
-    <MapContainer center={[60, 10]} zoom={5} scrollWheelZoom className="w-full h-full z-0">
+    <MapContainer center={[63.4, 10.4]} zoom={5} scrollWheelZoom className="h-[500px] w-full rounded-xl">
       <TileLayer
-        attribution='&copy; <a href="https://openstreetmap.org">OpenStreetMap</a>'
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {markører.map((m, i) => (
         <Marker key={i} position={[m.lat, m.lng]}>
-          <Popup>
-            <strong>{m.tittel}</strong><br />
-            {m.type}
-          </Popup>
+          <Popup>{m.tekst}</Popup>
         </Marker>
       ))}
     </MapContainer>
-  );
+  )
 }
