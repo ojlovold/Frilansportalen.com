2b7a57f79931650374e3f4af13e601e54144069b
ojlovold <ojlovold@gmail.com>
2025-05-11 17:40:44 +0200

    Update profil.tsx

--- pages/profil.tsx
+++ pages/profil.tsx
@@ -3,7 +3,8 @@ import { useUser } from "@supabase/auth-helpers-react";
 import Dashboard from "@/components/Dashboard";
 import Profilkort from "@/components/profil/Profilkort";
 import CVUpload from "@/components/profil/CVUpload";
-import Søknader from "@/components/profil/Søknader";
+import MineSøknader from "@/components/profil/MineSøknader";
+import SøkerStatistikk from "@/components/profil/SøkerStatistikk";
 import EksporterAltSomPDF from "@/components/profil/EksporterAltSomPDF";
 import EksporterGDPR from "@/components/profil/EksporterGDPR";
 
@@ -20,7 +21,8 @@ export default function ProfilSide() {
       <div className="space-y-6">
         <Profilkort userId={user.id} />
         <CVUpload userId={user.id} />
-        <Søknader userId={user.id} />
+        <SøkerStatistikk brukerId={user.id} />
+        <MineSøknader brukerId={user.id} />
         <EksporterAltSomPDF brukerId={user.id} />
         <EksporterGDPR brukerId={user.id} />
       </div>
