ad446701d26a710788b64c2594e9e64a3d44c7a8
ojlovold <ojlovold@gmail.com>
2025-05-11 22:27:15 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,8 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { SessionContextProvider } from "@supabase/auth-helpers-react";
-import { supabase } from "@/lib/supabaseClient";
-import TilgjengelighetsBar from "@/components/globalt/TilgjengelighetsBar";
+// pages/_app.tsx
+import 'leaflet/dist/leaflet.css'
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
-      <Component {...pageProps} />
-      <TilgjengelighetsBar />
-    </SessionContextProvider>
-  );
+  return <Component {...pageProps} />
 }
