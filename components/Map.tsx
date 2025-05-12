810ad009259472d3b7baa1605df8521a515bbb13
ojlovold <ojlovold@gmail.com>
2025-05-12 18:24:58 +0200

    Update Map.tsx

--- components/Map.tsx
+++ components/Map.tsx
@@ -4,34 +4,48 @@ import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
 import { useEffect } from 'react'
 
-// Fiks for manglende ikoner i produksjon
-delete (L.Icon.Default.prototype as any)._getIconUrl
+// Fjern standard ikonfeil i Leaflet
+delete (L.Icon.Default as any).prototype._getIconUrl
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: '/leaflet/marker-icon-2x.png',
-  iconUrl: '/leaflet/marker-icon.png',
-  shadowUrl: '/leaflet/marker-shadow.png',
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
 })
 
-type MapProps = {
-  markører: { lat: number; lng: number; tekst: string }[]
+type MarkerType = {
+  id: string
+  lat: number
+  lng: number
+  tittel: string
 }
 
-export default function Map({ markører }: MapProps) {
+export default function Map({ markorer }: { markorer: MarkerType[] }) {
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      import('leaflet')
-    }
+    // trigger resize fix
+    setTimeout(() => {
+      window.dispatchEvent(new Event('resize'))
+    }, 500)
   }, [])
 
+  const center = markorer.length
+    ? [markorer[0].lat, markorer[0].lng]
+    : [60.472, 8.468] // fallback: Norge
+
   return (
-    <MapContainer center={[63.4, 10.4]} zoom={5} scrollWheelZoom className="h-[500px] w-full rounded-xl">
+    <MapContainer
+      center={center as [number, number]}
+      zoom={6}
+      style={{ height: '500px', width: '100%' }}
+      scrollWheelZoom={true}
+    >
       <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
+        attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {markører.map((m, i) => (
-        <Marker key={i} position={[m.lat, m.lng]}>
-          <Popup>{m.tekst}</Popup>
+
+      {markorer.map((m) => (
+        <Marker key={m.id} position={[m.lat, m.lng]}>
+          <Popup>{m.tittel}</Popup>
         </Marker>
       ))}
     </MapContainer>
