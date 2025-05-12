29c7526dec9bfffa2f31c422611d3350b8e49e1e
ojlovold <ojlovold@gmail.com>
2025-05-12 18:57:33 +0200

    Update Map.tsx

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,7 +1,16 @@
 // components/Map.tsx
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { LatLngExpression } from "leaflet";
+import L, { LatLngExpression } from "leaflet";
+import { useEffect } from "react";
+
+// Fix for missing marker icons in Leaflet + Vite/Next.js
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
 
 interface MapProps {
   center: LatLngExpression;
@@ -12,6 +21,13 @@ interface MapProps {
 }
 
 export default function Map({ center, markers = [] }: MapProps) {
+  useEffect(() => {
+    // Leaflet fix for map height bug when container is hidden on load
+    setTimeout(() => {
+      window.dispatchEvent(new Event("resize"));
+    }, 100);
+  }, []);
+
   return (
     <MapContainer
       center={center}
