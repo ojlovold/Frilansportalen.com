e2b90d734d0b64d1520ca235e312beae2e8f6737
ojlovold <ojlovold@gmail.com>
2025-05-13 13:13:38 +0200

    Create Fagbibliotek.tsx

---
+++ components/fag/Fagbibliotek.tsx
@@ -0,0 +1,8 @@
+export default function Fagbibliotek() {
+  return (
+    <div className="p-6 bg-white rounded shadow">
+      <h1 className="text-2xl font-bold mb-4">Fagbibliotek</h1>
+      <p>Her vil du finne relevante artikler og dokumenter.</p>
+    </div>
+  );
+}
