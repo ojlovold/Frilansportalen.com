065c576e2b145b8b7c230cb81bfdd50349b3d76f
ojlovold <ojlovold@gmail.com>
2025-05-13 19:44:14 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,20 @@
 // pages/_app.tsx
 import { useState } from 'react'
+import { UserContextProvider } from '@supabase/auth-helpers-react'
+import { createClient } from '@supabase/supabase-js'
 import type { AppProps } from 'next/app'
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import 'leaflet/dist/leaflet.css'
 import '../styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
 
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionContextProvider
-      supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}
-    >
+    <UserContextProvider supabaseClient={supabase}>
       <Component {...pageProps} />
-    </SessionContextProvider>
+    </UserContextProvider>
   )
 }
