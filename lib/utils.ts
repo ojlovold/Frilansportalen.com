6809322d1fb7b626b0d3ad2aa6a4f4f94b8e224d
ojlovold <ojlovold@gmail.com>
2025-05-14 22:08:39 +0200

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,16 +1,15 @@
 export function formatDate(dateString: string): string {
-  const date = new Date(dateString)
-  return date.toLocaleDateString('no-NO')
+  const date = new Date(dateString);
+  return date.toLocaleDateString("no-NO");
 }
 
 export function formatCurrency(nok: number): string {
-  return new Intl.NumberFormat('no-NO', {
-    style: 'currency',
-    currency: 'NOK',
-  }).format(nok)
+  return new Intl.NumberFormat("no-NO", {
+    style: "currency",
+    currency: "NOK",
+  }).format(nok);
 }
 
-// Legger til for button.tsx:
 export function cn(...classes: (string | undefined | null | false)[]) {
   return classes.filter(Boolean).join(" ");
 }
