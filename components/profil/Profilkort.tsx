549ca536d9f15b2c5fa78ecf66a5371538e5b5a3
ojlovold <ojlovold@gmail.com>
2025-05-17 11:40:22 +0200

    Update Profilkort.tsx

--- components/profil/Profilkort.tsx
+++ components/profil/Profilkort.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import supabase from "@/lib/supabaseClient";
+import { supabase } from "@/lib/supabaseClient";
 
 export default function Profilkort({ userId }: { userId: string }) {
   const [profil, setProfil] = useState<any>({
