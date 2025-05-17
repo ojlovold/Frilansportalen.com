35350bfe052ccf149b5a07b581a2d3bc95f20074
ojlovold <ojlovold@gmail.com>
2025-05-17 11:32:38 +0200

    Update MineSøknader.tsx

--- components/profil/MineSøknader.tsx
+++ components/profil/MineSøknader.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import supabase from "@/lib/supabaseClient";
+import { supabase } from "@/lib/supabaseClient";
 
 export default function MineSøknader({ brukerId }: { brukerId: string }) {
   const [søknader, setSøknader] = useState<any[]>([]);
