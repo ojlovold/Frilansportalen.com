44edb244855d0046f28f87942bc0d43c9097a2dc
ojlovold <ojlovold@gmail.com>
2025-05-18 12:01:34 +0200

    Update getNotifications.ts

--- lib/getNotifications.ts
+++ lib/getNotifications.ts
@@ -1,4 +1,4 @@
-import supabase from "./supabaseClient";
+import { supabase } from "./supabaseClient";
 
 export default async function getNotifications(userId: string) {
   const { data, error } = await supabase
