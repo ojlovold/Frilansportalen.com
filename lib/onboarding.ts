3656fe9b42397bbadeeadd0a362eb1fc8e1a9ff1
ojlovold <ojlovold@gmail.com>
2025-05-18 12:13:21 +0200

    Update onboarding.ts

--- lib/onboarding.ts
+++ lib/onboarding.ts
@@ -1,10 +1,10 @@
-import supabase from './supabaseClient'
+import { supabase } from './supabaseClient';
 
 export async function fullforOnboarding(userId: string) {
   const { error } = await supabase
     .from('brukerprofiler')
     .update({ onboarding_fullfort: true })
-    .eq('id', userId)
+    .eq('id', userId);
 
-  if (error) throw error
+  if (error) throw error;
 }
