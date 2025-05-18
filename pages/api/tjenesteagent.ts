bdc245433e9f5da8154241f47ec9888cce76cea1
ojlovold <ojlovold@gmail.com>
2025-05-18 13:34:11 +0200

    Update tjenesteagent.ts

--- pages/api/tjenesteagent.ts
+++ pages/api/tjenesteagent.ts
@@ -1,6 +1,6 @@
 // pages/api/tjenesteagent.ts
 import { NextApiRequest, NextApiResponse } from 'next'
-import supabase from '../../lib/supabaseClient'
+import { supabase } from '../../lib/supabaseClient'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { tjeneste } = req.body
