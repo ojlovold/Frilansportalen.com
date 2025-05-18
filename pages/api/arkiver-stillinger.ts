61d4dbba0b5b4e17e7cfab4a4a61809e7cadbb98
ojlovold <ojlovold@gmail.com>
2025-05-18 13:04:22 +0200

    Update arkiver-stillinger.ts

--- pages/api/arkiver-stillinger.ts
+++ pages/api/arkiver-stillinger.ts
@@ -1,5 +1,6 @@
+// pages/api/arkiver-stillinger.ts
 import type { NextApiRequest, NextApiResponse } from "next";
-import supabase from "@/lib/supabaseClient";
+import { supabase } from "@/lib/supabaseClient";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") return res.status(405).end();
