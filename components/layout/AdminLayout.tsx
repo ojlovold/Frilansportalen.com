e6a27221addfc1a9133ab954402370160539b3c9
ojlovold <ojlovold@gmail.com>
2025-05-19 21:53:54 +0200

    Update AdminLayout.tsx

--- components/layout/AdminLayout.tsx
+++ components/layout/AdminLayout.tsx
@@ -3,17 +3,20 @@ import Head from "next/head";
 import TilbakeKnapp from "@/components/TilbakeKnapp";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import useDesignFarger from "@/hooks/useDesignFarger";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   const { pathname } = useRouter();
   const title = pathname.split("/").pop()?.replace(/-/g, " ") ?? "Admin";
+
+  const { bakgrunnsfarge, tekstfarge } = useDesignFarger();
 
   return (
     <>
       <Head>
         <title>{title} | Frilansportalen Admin</title>
       </Head>
-      <main className="min-h-screen bg-portalGul text-black p-6 max-w-6xl mx-auto">
+      <main className={`min-h-screen ${bakgrunnsfarge} ${tekstfarge} p-6 max-w-6xl mx-auto`}>
         {pathname !== "/admin" && <TilbakeKnapp />}
         <h1 className="text-3xl font-bold mb-6 capitalize">{title}</h1>
         {children}
