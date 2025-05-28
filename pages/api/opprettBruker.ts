0047c0964c10f5b759259b032e91c7e6b589bb0c
ojlovold <ojlovold@gmail.com>
2025-05-28 23:43:48 +0200

    Update opprettBruker.ts

--- pages/api/opprettBruker.ts
+++ pages/api/opprettBruker.ts
@@ -1,10 +1,9 @@
-// pages/api/opprettBruker.ts
 import { NextApiRequest, NextApiResponse } from "next";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY! // Må være satt i Vercel eller .env.local
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
