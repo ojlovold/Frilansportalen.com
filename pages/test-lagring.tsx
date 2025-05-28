ca65f1ac50fe9a0479b440c8834bed8fb467fe3d
ojlovold <ojlovold@gmail.com>
2025-05-28 20:40:26 +0200

    Create test-lagring.tsx

---
+++ pages/test-lagring.tsx
@@ -0,0 +1,10 @@
+import TestLagring from "../components/TestLagring";
+
+export default function Testside() {
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-bold mb-4">Test: Kvitteringslagring</h1>
+      <TestLagring />
+    </div>
+  );
+}
