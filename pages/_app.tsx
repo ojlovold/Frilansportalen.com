c1a97d047f40f5ae303ffc9868460f935ea31f03
ojlovold <ojlovold@gmail.com>
2025-06-05 19:44:11 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,20 +4,28 @@ import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import AdminLayout from "@/components/layout/AdminLayout";
-import Layout from "@/components/Layout"; // ← LEGG TIL DENNE
+import Layout from "@/components/Layout";
 import "leaflet/dist/leaflet.css";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
   const router = useRouter();
+
   const isAdmin = router.pathname.startsWith("/admin");
+  const isForside = router.pathname === "/";
 
   const content = <Component {...pageProps} />;
 
   return (
     <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
-      {isAdmin ? <AdminLayout>{content}</AdminLayout> : <Layout>{content}</Layout>}
+      {isAdmin ? (
+        <AdminLayout>{content}</AdminLayout>
+      ) : isForside ? (
+        content
+      ) : (
+        <Layout>{content}</Layout>
+      )}
     </SessionContextProvider>
   );
 }
