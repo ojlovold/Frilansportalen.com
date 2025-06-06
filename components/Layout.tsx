8afbd5400b1034a0cb52426473f80bc6920fe240
ojlovold <ojlovold@gmail.com>
2025-06-06 20:04:01 +0200

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -157,9 +157,7 @@ export default function Layout({ children }: { children: ReactNode }) {
 
       
 
-<style>{`html, body, #__next { outline: 2px solid red !important; pointer-events: auto !important; position: relative !important; z-index: 0 !important; }`}</style>
-
-$1"p-4 max-w-5xl mx-auto">
+<main className="p-4 max-w-5xl mx-auto">
   {children}
 </main>
     </div>
